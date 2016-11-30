f2d983e86c72699fe9b16ce3138107193250350a
Devin Cody <devincody@gmail.com>
2016-11-30 00:22:54 -0500

    added XY control, added some nice plots, added fomatting, need to tune angle PID control before XY control can continue

--- quadrotor_ode.m
+++ quadrotor_ode.m
@@ -6,7 +6,7 @@
 
 function [dy] = quadrotor_ode(t,y)
 
-global T m g Ax Ay Az C J w1 w2 w3 w4 l k b Ixx Iyy Izz
+global T m g M Ax Ay Az C J w1 w2 w3 w4 l k b Ixx Iyy Izz
 
 %%%%%%%%%%%% CALCULATE new w1-w4 USING QUADROTOR_PID.M %%%%%%%%%%%%
 %{
@@ -26,18 +26,18 @@ psi_dot = y(12)
 % can't use dy(2),dy(4),dy(6) (why?), so just calculate their values to
 % pass through to quadrotor_pid.m
 quadrotor_pid(t,y(1),y(2),((T/m) * (cos(y(11))*sin(y(9))*cos(y(7)) + sin(y(11))*sin(y(7)))) - (1/m)*Ax*y(2),y(3),y(4),((T/m) * (sin(y(11))*sin(y(9))*cos(y(7)) - cos(y(11))*sin(y(7)))) - (1/m)*Ay*y(4),y(5),y(6),-g + ((T/m) * (cos(y(9))*cos(y(7)))) - (1/m)*Az*y(6),y(7),y(8),y(9),y(10),y(11),y(12));
-
-% update combined forces of rotors create thrust T in direction of z-axis
+t
+%% update combined forces of rotors create thrust T in direction of z-axis
 T = k*(w1^2 + w2^2 + w3^2 + w4^2);
 
 
-% update tauB used to calculate angular accelerations
+%% update tauB used to calculate angular accelerations
 tauB(1) = l*k*(-w2^2 + w4^2); % for + rotor configuration (as opposed to x)
 tauB(2) = l*k*(-w1^2 + w3^2); % for + rotor configuration (as opposed to x)
 tauB(3) = b*w1^2 - b*w2^2 + b*w3^2 - b*w4^2; % effect of wi_dot is omitted (considered small) - also, rotors 2 and 4 spin in - direction
 
 
-% update C matrix used to calculate angular accelerations
+%% update C matrix used to calculate angular accelerations
 C(1,1) = 0;
 C(1,2) = (Iyy - Izz)*(y(10)*cos(y(7))*sin(y(7)) + y(12)*sin(y(7))^2*cos(y(9))) + ...
          (Izz - Iyy)*(y(12)*cos(y(7))^2*cos(y(9))) - ...
@@ -65,7 +65,7 @@ C(3,3) = (Iyy - Izz)*y(8)*cos(y(7))*sin(y(7))*(cos(y(9))^2) - ...
          Ixx*y(10)*cos(y(9))*sin(y(9));
 
 
-% update Jacobian used to calculate angular accelerations
+%% update Jacobian used to calculate angular accelerations
 J(1,1) = Ixx;
 J(1,2) = 0;
 J(1,3) = -Ixx*sin(y(9));
@@ -79,7 +79,7 @@ J(3,2) = (Iyy - Izz)*cos(y(7))*sin(y(7))*cos(y(9));
 J(3,3) = (Ixx*sin(y(9))^2) + (Iyy*(sin(y(7))^2)*(cos(y(9))^2)) + (Izz*(cos(y(7))^2)*(cos(y(9))^2));
 
 
-% calculate phi_dot_dot, theta_dot_dot, psi_dot_dot
+%% calculate phi_dot_dot, theta_dot_dot, psi_dot_dot
 ang_vel = [y(8) y(10) y(12)]'; % angular velocities
 ang_accel = J\(tauB' - C*ang_vel); % angular accelerations
 
@@ -114,10 +114,10 @@ dy(11) = y(12);
 dy(12) = ang_accel(3);
 
 % don't allow negative altitude
-if y(5) < 0.001
-    dy(5) = max([dy(5) 0]);
-    dy(6) = max([dy(6) 0]);
-end
+% if y(5) < 0.001
+%     dy(5) = max([dy(5) 0]);
+%     dy(6) = max([dy(6) 0]);
+% end
 
 dy=dy';
 
--- quadrotor_sim.m
+++ quadrotor_sim.m
@@ -7,33 +7,39 @@
 clear all %#ok<CLALL>
 syms time_sym
 
-global T m g Ax Ay Az C J l k b Ixx Iyy Izz w1 w2 w3 w4 ...
+global T m g M Ax Ay Az C J l k b Ixx Iyy Izz w1 w2 w3 w4 ...
     desired_x desired_y desired_z ...
     desired_x_dot desired_y_dot desired_z_dot ...
     desired_x_ddot desired_y_ddot desired_z_ddot ...
     K_p_z K_d_z K_p_phi K_d_phi K_p_theta K_d_theta K_p_psi K_d_psi ...
     K_i_z K_i_phi K_i_theta K_i_psi ...
+    K_p_y K_d_y K_p_x K_d_x ...
     phi_int_err theta_int_err psi_int_err z_int_err ...
+    phi_store theta_store psi_store ...
     w1_store w2_store w3_store w4_store t_store
 
 
 g = 9.81;
+M = [k k k k ; 0 -l*k 0 l*k; -l*k 0 l*k 0; b -b b -b]; % mixer matrix
 
 %%%%%%%% Variables which may be changed %%%%%%%%
 
-% define desired trajectory as symbolic function of time (variable time_sym)
-desired_x_sym(time_sym) = 0.*time_sym; % as of now, must be zero
-desired_y_sym(time_sym) = 0.*time_sym; % as of now, must be zero
+%% define desired trajectory as symbolic function of time (variable time_sym)
+
 % desired_z_sym(time_sym) = 0.*time_sym;
 % desired_z_sym(time_sym) = 5./(1+exp(8-time_sym));
 % desired_z_sym(time_sym) = 10 + 0.*time_sym;
 % sigma = 1.5;
 sigma = 3;
-mu = 10;
-amp = 100;
-desired_z_sym(time_sym) = amp*(1/sqrt(2*sigma^2*pi))*exp(-(time_sym-mu)^2/(2*sigma^2));
+mu = 5;
+amp = 10;
+desired_x_sym(time_sym) = 0*25*(1/sqrt(2*sigma^2*pi))*exp(-(time_sym-15)^2/(2*sigma^2)); % as of now, must be zero
+desired_y_sym(time_sym) = 0+0*time_sym;
+desired_z_sym(time_sym) = 0*amp*(1/sqrt(3*sigma^2*pi))*exp(-(time_sym-8)^2/(3*sigma^2));
 
-K_i_z = .0005;
+%% PID Gains
+% !!!! Only the Z gains have been tuned !!!!
+K_i_z = .0007;
 K_p_z = 8.85*0.468;
 K_d_z = 3*0.468;
 
@@ -49,14 +55,20 @@ K_i_psi = 3*10^-5;
 K_p_psi = 3*10^-3;
 K_d_psi = .75*10^-3;
 
+K_p_y = .01;
+K_d_y = .007;
+K_p_x = K_p_y;
+K_d_x = K_d_y;
 
-sim_time = 20; % simulation runtime in seconds
+%% plot settings
+linewidth = 1.5;
+sim_time = 4; % simulation runtime in seconds
 animation_select = 0; % 0: no animation; 1: full motion, one central thrust vector
                       % 2: fixed at origin (only see angular position), one central thrust vector
                       % 3: full motion, four thrust vectors (one for each motor)
                       % 4: fixed at origin (only see angular position), four thrust vectors
 
-%%% rotor angular velocities (rad/s)
+%% [!!DEPRECIATED!!] rotor angular velocities (rad/s)
 % with original settings from 'Modelling and control of quadcopter' (Teppo Luukkonen):
 % wi > 620.61 will cause it to rise
 % rotors 2 and 4 spin in - direction, 1 and 3 in + direction
@@ -73,18 +85,18 @@ animation_select = 0; % 0: no animation; 1: full motion, one central thrust vect
 % w3 = @(t) (c+300)*heaviside(t-.5);
 % w4 = @(t) c*heaviside(t-.5);
 
-%%% Various constants
+%% Various constants
 k = 2.980*10^-6; % lift constant
 m = 0.468; % mass (kg)
 l = 0.225; % distance between rotor and center of mass of quad (m)
 b = 1.140*10^-7; % drag constant
 
-%%% Inertia matrix (kg * m^2)
+%% Inertia matrix (kg * m^2)
 Ixx = 4.856*10^-3;
 Iyy = 4.856*10^-3;
 Izz = 8.801*10^-3;
 
-%%% Drag force coefficients for velocities (kg/s)
+%% Drag force coefficients for velocities (kg/s)
 % Ax = 0.25;
 % Ay = 0.25;
 % Az = 0.25;
@@ -92,7 +104,7 @@ Ax = 0;
 Ay = 0;
 Az = 0;
 
-%%% Initial conditions
+%% Initial conditions
 x0 = 0;
 x_dot0 = 0;
 
@@ -102,7 +114,7 @@ y_dot0 = 0;
 z0 = 0;
 z_dot0 = 0;
 
-% the following are initial angles in radians
+%% the following are initial angles in radians
 phi0 = 0;
 phi_dot0 = 0;
 
@@ -112,14 +124,7 @@ theta_dot0 = 0;
 psi0 = 0;
 psi_dot0 = 0;
 
-%%% plot settings
-linewidth = 1.5;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% create anonymous functions for desired position, velocity, acceleration
-% profiles
+%% create anonymous functions for desired position, velocity, acceleration profiles
 desired_x = matlabFunction(desired_x_sym);
 desired_y = matlabFunction(desired_y_sym);
 desired_z = matlabFunction(desired_z_sym);
@@ -140,19 +145,14 @@ desired_x_ddot = matlabFunction(desired_x_ddot_sym);
 desired_y_ddot = matlabFunction(desired_y_ddot_sym);
 desired_z_ddot = matlabFunction(desired_z_ddot_sym);
 
-phi_int_err = 0;
-theta_int_err = 0;
-psi_int_err = 0;
-z_int_err = 0;
-
-% initialize matrices used to store values of motor angular velocities
+%% initialize matrices used to store values of motor angular velocities
 t_store = [];
 w1_store = [];
 w2_store = [];
 w3_store = [];
 w4_store = [];
 
-% initialize rotor angular velocities
+%% initialize rotor angular velocities
 w1 = 0;
 w2 = 0;
 w3 = 0;
@@ -171,40 +171,38 @@ y = [x0 x_dot0 ...
      psi0 psi_dot0];
 
 
-% initialize C and J matrices used to calculate angular accelerations
+%% initialize C and J matrices used to calculate angular accelerations
 C = zeros(3);
 J = zeros(3);
 
+%% RUN SIMULATION
 time = [0, sim_time];
 options = odeset('RelTol',1e-7,'AbsTol',1e-7,'Stats','on');
 [t,y] = ode45(@quadrotor_ode,time,y,options);
 
-%%% Plot
+%% PLOT XYZ data
 figure('units','normalized','outerposition',[0 0 1 1])
-
-% plot positions
-% subplot(511)
-% plot(t,y(:,1), 'LineWidth', linewidth) % x
-% hold on
-% plot(t,desired_x(t), 'LineWidth', linewidth) % desired x
-% legend('x', 'desired x')
-% grid on
-% title('X Position vs. Time')
-% xlabel('time (s)')
-% ylabel('position (m)')
-% 
-% subplot(512)
-% plot(t,y(:,3), 'LineWidth', linewidth) % y
-% hold on
-% plot(t,desired_y(t), 'LineWidth', linewidth) % desired y
-% legend('y', 'desired y')
-% grid on
-% title('Y Position vs. Time')
-% xlabel('time (s)')
-% ylabel('position (m)')
-
-% subplot(513)
 subplot(311)
+plot(t,y(:,1), 'LineWidth', linewidth) % x
+hold on
+plot(t,desired_x(t), 'LineWidth', linewidth) % desired x
+legend('x', 'desired x')
+grid on
+title('X Position vs. Time')
+xlabel('time (s)')
+ylabel('position (m)')
+
+subplot(312)
+plot(t,y(:,3), 'LineWidth', linewidth) % y
+hold on
+plot(t,desired_y(t), 'LineWidth', linewidth) % desired y
+legend('y', 'desired y')
+grid on
+title('Y Position vs. Time')
+xlabel('time (s)')
+ylabel('position (m)')
+
+subplot(313)
 plot(t,y(:,5), 'LineWidth', linewidth) % z
 hold on
 plot(t,desired_z(t), 'LineWidth', linewidth) % desired z
@@ -214,30 +212,49 @@ title('Z Position vs. Time')
 xlabel('time (s)')
 ylabel('position (m)')
 
-
-% subplot(514)
-subplot(312)
+%% PLOT ANGLES
+figure('units','normalized','outerposition',[0 0 1 1])
+subplot(311)
 plot(t,y(:,7), 'LineWidth', linewidth) % phi
 hold on
-plot(t,y(:,9), 'LineWidth', linewidth) % theta
-hold on
-plot(t,y(:,11), 'LineWidth', linewidth) % psi
-legend('\phi','\theta','\psi')
+plot(t_store, phi_store, 'LineWidth', linewidth) % desired phi
+legend('roll', 'desired roll')
 grid on
 title('Angular Position vs. Time')
 xlabel('time (s)')
 ylabel('angular position (rad)')
 
-% filter out duplicates (failed attempts of ode solver) in the stored w_i and time arrays
-unique_ind = boolean(sum(t_store == t));
-t_store = t_store(unique_ind);
-w1_store = w1_store(unique_ind);
-w2_store = w2_store(unique_ind);
-w3_store = w3_store(unique_ind);
-w4_store = w4_store(unique_ind);
+subplot(312)
+plot(t,y(:,9), 'LineWidth', linewidth) % theta
+hold on
+plot(t_store, theta_store, 'LineWidth', linewidth) % desired theta
+legend('pitch', 'desired pitch')
+grid on
+title('Angular Position vs. Time')
+xlabel('time (s)')
+ylabel('angular position (rad)')
 
-% subplot(515)
 subplot(313)
+plot(t,y(:,11), 'LineWidth', linewidth) % psi
+hold on
+plot(t_store, psi_store, 'LineWidth', linewidth) % desired psi
+legend('yaw', 'desired yaw')
+grid on
+title('Angular Position vs. Time')
+xlabel('time (s)')
+ylabel('angular position (rad)')
+
+%% Plot Motor input
+% filter out duplicates (failed attempts of ode solver) in the stored w_i and time arrays
+% unique_ind = boolean(sum(t_store == t));
+% t_store = t_store(unique_ind);
+% w1_store = w1_store(unique_ind);
+% w2_store = w2_store(unique_ind);
+% w3_store = w3_store(unique_ind);
+% w4_store = w4_store(unique_ind);
+
+figure('units','normalized','outerposition',[0 0 1 1])
+subplot(111)
 plot(t_store, w1_store, 'LineWidth', linewidth)
 hold on
 plot(t_store, w2_store, 'LineWidth', linewidth)
@@ -251,7 +268,7 @@ title('Motor Angular Velocities vs. Time')
 xlabel('time (s)')
 ylabel('angular velocity (rad/sec)')
 
-
+%% Animation stuff
 % if animation_select == 1
 %     % animate quad motion - view_quad has one thrust vector attached to center
 %     % of mass
